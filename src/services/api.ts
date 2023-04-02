6b16f5a2af969ba498a695c85e94c7c672708a99
Felipe Santana <felipe.jesus0801@gmail.com>
2023-04-02 00:22:42 -0300

    resolving error axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "https://felipesantana.vercel.app/api/services/",
+  baseURL: "felipesantana-7i9adokph-feelipesantana.vercel.app/api/services/",
   headers: {
     key: "Access-Control-Allow-Origin", value: "*" 
   }
