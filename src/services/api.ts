c82e1c0f21803658005acad6471150af4f66ec77
Felipe Santana <felipe.jesus0801@gmail.com>
2023-04-02 00:17:56 -0300

    resolving error axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "/api/services",
+  baseURL: "/api/services/",
+  headers: {
+    key: "Access-Control-Allow-Origin", value: "*" 
+  }
 })
