05e32e810c3c71f0ab18505a87647df571d9a4b6
Felipe Santana <felipe.jesus0801@gmail.com>
2023-04-02 00:07:09 -0300

    resoving error axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,4 @@ import axios from 'axios'
 
 export const api = axios.create({
   baseURL: "http://127.0.0.1:3000/api/services",
-  headers: {
-    key: "Access-Control-Allow-Origin", value: "*" 
-  }
 })
