d49b4419e0882d1c077d04464807c6f4d878e919
Felipe Santana <felipe.jesus0801@gmail.com>
2023-04-01 23:55:43 -0300

    upt: axios route

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:3000/api/services/",
+  baseURL: "/api/services/",
   headers: {
     key: "Access-Control-Allow-Origin", value: "*" 
   }
