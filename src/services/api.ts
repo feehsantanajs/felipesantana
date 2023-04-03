4ff5d7d1c5e73b37f92c16fc58d5bce67f8f5f6f
Felipe Santana <felipe.jesus0801@gmail.com>
2023-04-03 17:56:43 -0300

    ip`

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.API_ROUTE_LOCAL,
+  baseURL: '/api/services/',
   headers: {
     key: "Access-Control-Allow-Origin", value: "*" 
   }
