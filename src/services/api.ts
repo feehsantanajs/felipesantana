f92bbf4dac2269679fc344e5cc830654db387b4a
Felipe Santana <felipe.jesus0801@gmail.com>
2023-04-03 17:49:36 -0300

    upt: api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "https://felipesantana-7i9adokph-feelipesantana.vercel.app/api/services/",
+  baseURL: process.env.API_ROUTE_LOCAL,
   headers: {
     key: "Access-Control-Allow-Origin", value: "*" 
   }
