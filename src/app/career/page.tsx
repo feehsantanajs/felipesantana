049b7a1e8ca8ef8fbc57fb25f82529641d1572fc
Felipe Santana <felipe.jesus0801@gmail.com>
2023-04-03 19:56:41 -0300

    upt

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -12,7 +12,7 @@ type DatasCareerProps = [{
 
 export default async function Career(){
 
-  const response = await axios.get('https://felipesantana.vercel.app/api/experiences')
+  const response = await axios.get('/api/experiences')
   const datas:DatasCareerProps = await response.data
   
   return(
